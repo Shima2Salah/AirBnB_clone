3dc5122a8029080a0e578efa67d23fc0887cf041
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-01-11 07:53:55 -0800

    Airbnb

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Parent class Module representation"""
+from datetime import datetime
+from uuid import uuid4
+from models import storage
+
+
+class BaseModel:
+    """Parent BaseModel class"""
+
+    def __init__(self, *args, **kwargs):
+        """BaseModel intialization
+        args: any tuple
+        kwargs: any dictionary item"""
+    
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs is not None and kwargs != {}:
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = val
+        else:
+            storage.new(self)
+
+    def __str__(self):
+        """print class name, id ,dictionary"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """method saves date and time updates"""
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """method to get all dictionary keys/values"""
+        mydict = self.__dict__.copy()
+        mydict["created_at"] = self.created_at.isoformat()
+        mydict["updated_at"] = self.updated_at.isoformat()
+        mydict["__class__"] = self.__class__.__name__
+        return mydict
